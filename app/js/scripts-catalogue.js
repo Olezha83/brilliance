3c117708df9a5477496507904cfa87e1281b8fd1
Olezha83 <oleg.it.workbox@gmail.com>
2025-03-28 19:27:13 +0200

    Add 'Remove all' button for applied filters (markup and logic)

--- app/js/scripts-catalogue.js
+++ app/js/scripts-catalogue.js
@@ -78,6 +78,8 @@ const filtersInputs = Array.from(
 const filtersApplied = document
   .getElementsByClassName('gallery__filters-on')[0]
 
+const removeAllFiltersBtn = document.getElementsByClassName('gallery__remove-btn')[0]
+
 function createFilter(name) {
   const filterWrap = document.createElement('div')
   filterWrap.classList.add('gallery__filter-on')
@@ -99,17 +101,31 @@ const filterElements = {}
 filtersInputs.forEach(input => input.addEventListener('change', function() {
   const labelText = input.nextElementSibling.innerText
   if (input.checked) {
+    Object.keys(filterElements).length === 0 && 
+      removeAllFiltersBtn.classList.remove('hidden')
+ 
+    removeAllFiltersBtn.addEventListener('click', function () {
+      const crossIcons = Array.from(
+        filtersApplied.getElementsByClassName('gallery__remove-filter')
+      )
+      crossIcons.forEach(crossIcon => crossIcon.click())
+    }, {once: true})
+    
     const newElem = filtersApplied.appendChild(createFilter(labelText))
     filterElements[input.id] = newElem
     newElem.lastElementChild.addEventListener('click', function () {
       newElem.remove()
       input.checked = false
+      Object.keys(filterElements).length === 1 && 
+        removeAllFiltersBtn.classList.add('hidden')
       delete filterElements[input.id]
     })
 
   } else {
     const newElem = filterElements[input.id]
     filtersApplied.removeChild(newElem)
+    Object.keys(filterElements).length === 1 && 
+      removeAllFiltersBtn.classList.add('hidden')
     delete filterElements[input.id]
   }
 }))
